f6d46b33fd3d9fd46b94d54cfc9863a2e1859ac3
Dmytro Khvedchuk <dmytro.khvedchuk.dev@gmail.com>
2025-08-20 20:55:33 +0300

    added schemas with data types for klines and trades

--- utils/global_variables/SCHEMAS.py
+++ utils/global_variables/SCHEMAS.py
@@ -19,3 +19,28 @@ TIBS_SCHEMA = {
     "first_trade_id": pl.Int64,
     "last_trade_id": pl.Int64,
 }
+
+KLINES_SCHEMA = {
+    "open_time": pl.Int64,
+    "open": pl.Float64,
+    "high": pl.Float64,
+    "low": pl.Float64,
+    "close": pl.Float64,
+    "volume": pl.Float64,
+    "close_time": pl.Int64,
+    "quote_asset_volume": pl.Float64,
+    "num_trades": pl.Int64,
+    "taker_buy_base_asset_volume": pl.Float64,
+    "taker_buy_quote_asset_volume": pl.Float64,
+    "ignore": pl.Utf8,  # always "0", but kept as string
+}
+
+TRADES_SCHEMA = {
+    "trade_id": pl.Int64,
+    "price": pl.Float64,
+    "qty": pl.Float64,
+    "quote_qty": pl.Float64,
+    "time": pl.Int64,
+    "is_buyer_maker": pl.Boolean,
+    "is_best_match": pl.Boolean,
+}
