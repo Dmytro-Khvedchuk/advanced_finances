051a182f646cdcb6e170b54199ac8eb1f0e592e9
Dmytro Khvedchuk <dmytro.khvedchuk.dev@gmail.com>
2025-08-21 19:20:43 +0300

    added client for credentials

---
+++ engine/apps/data_managers/clickhouse/client.py
@@ -0,0 +1,13 @@
+from os import getenv
+from clickhouse_driver import Client
+
+def get_clickhouse_client() -> Client:
+    password = getenv("CLICKHOUSE_PASSWORD") or None
+
+    return Client(
+        host=getenv('CLICKHOUSE_HOST'),
+        port=int(getenv('CLICKHOUSE_PORT')),
+        user=getenv('CLICKHOUSE_USER'),
+        password=password,
+        database=getenv('CLICKHOUSE_DB'),
+    )
