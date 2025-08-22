17821f79bdd0623d2937fa6e9ab0e54bb4ce79a7
Dmytro Khvedchuk <dmytro.khvedchuk.dev@gmail.com>
2025-08-22 14:31:30 +0300

    refactored klines for reducing file size

---
+++ engine/apps/data_managers/managers/klines_manager.py
@@ -0,0 +1,152 @@
+from utils.global_variables.GLOBAL_VARIABLES import (
+    SYMBOL,
+    BINANCE_TRADES_LIMIT,
+    TIMEFRAME,
+    TIMEFRAME_MAP,
+    BINANCE_EARLIEST_DATE,
+    BINANCE_LATEST_DATE,
+)
+from utils.global_variables.SCHEMAS import KLINES_SCHEMA
+from dateutil.parser import parse
+import polars as pl
+from utils.logger.logger import LoggerWrapper
+from utils.logger.logger import log_execution
+import numpy as np
+from tqdm import tqdm
+from clickhouse_driver import Client as DBClient
+from engine.apps.data_managers.clickhouse.data_manager import ClickHouseDataManager
+from binance.client import Client as BinanceClient
+from API.data_fetcher import FetchData
+
+
+class KlineManager:
+    def __init__(
+        self,
+        database_client: DBClient,
+        binance_client: BinanceClient,
+        symbol: str = SYMBOL,
+        log_level: int = 10,
+    ):
+        self.logger = LoggerWrapper(name="Kline Manager Module", level=log_level)
+        self.symbol = symbol
+        self.click_house_data_manager = ClickHouseDataManager(
+            client=database_client, log_level=log_level
+        )
+        self.data_fetcher = FetchData(
+            client=binance_client, symbol=symbol, log_level=log_level
+        )
+
+    # TODO: refactor
+    @log_execution
+    def get_klines(
+        self,
+        *,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        timeframe: str = TIMEFRAME,
+    ):
+        if start_date:
+            start_date = self._parse_date_for_klines(start_date)
+        if end_date:
+            end_date = self._parse_date_for_klines(end_date)
+
+        present_time_in_db = pl.DataFrame(
+            self.click_house_data_manager.get_klines(
+                symbol=self.symbol,
+                timeframe=timeframe,
+                start_date=start_date,
+                end_date=end_date,
+                columns=["open_time"],
+            ),
+            schema=["open_time"],
+            orient="row",
+        )["open_time"].to_numpy()
+
+        expected_time_in_db = self._generate_expected_timestamps(
+            start_date=start_date, end_date=end_date, timeframe=timeframe
+        )
+
+        timestamps_to_fetch = np.setxor1d(present_time_in_db, expected_time_in_db)
+        timestamps_to_fetch = np.sort(timestamps_to_fetch)
+
+        if timestamps_to_fetch.size > 0:
+            self.logger.info("Missing data in the dataframe. Fetching...")
+            elements = {}
+            N = BINANCE_TRADES_LIMIT
+            for i in range(0, len(timestamps_to_fetch), N):
+                from_ts = timestamps_to_fetch[i]
+                to_ts = timestamps_to_fetch[min(i + N, len(timestamps_to_fetch)) - 1]
+                elements[from_ts] = to_ts
+
+            self._fetch_and_write_klines(fetch_dictionary=elements, timeframe=timeframe)
+
+        data = pl.DataFrame(
+            self.click_house_data_manager.get_klines(
+                symbol=self.symbol,
+                timeframe=timeframe,
+                start_date=start_date,
+                end_date=end_date,
+            ),
+            schema=KLINES_SCHEMA,
+            orient="row",
+        )
+
+        return data
+
+    @log_execution
+    def _fetch_and_write_klines(
+        self, fetch_dictionary: dict, timeframe: str = TIMEFRAME
+    ):
+        interval_ms = TIMEFRAME_MAP[timeframe]  # перетворення 1h → 3600000 ms
+        for from_ts, to_ts in tqdm(fetch_dictionary.items(), desc="Fetching klines"):
+            start = from_ts
+            while start <= to_ts:
+                data = pl.DataFrame(
+                    self.data_fetcher.fetch_historical_klines(
+                        timeframe=timeframe, start_str=start, end_str=to_ts
+                    ),
+                    orient="row",
+                    schema=KLINES_SCHEMA,
+                )
+                if len(data) == 0:
+                    break
+
+                self.click_house_data_manager.insert_klines(df=data, symbol=self.symbol)
+
+                start = int(data["open_time"].max()) + interval_ms
+
+    # ---=== HELPER METHODS ===---
+    def _parse_date_for_klines(self, date: str = "22 Oct 2024"):
+        try:
+            parsed_date = parse(date)
+            timestamp_ms = int(parsed_date.timestamp() * 1000)
+            return timestamp_ms
+        except Exception as fallback_error:
+            self.logger.error(f"Failed to parse date '{date}': {fallback_error}. ")
+            raise ValueError(f"Failed to parse date '{date}': {fallback_error}. ")
+
+    # ---=== STATIC METHODS ===---
+    def _generate_expected_timestamps(
+        self,
+        *,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        timeframe: str = TIMEFRAME,
+    ) -> np.ndarray:
+        """
+        Generate expected timestamps (in ms) between start_date and end_date
+        based on timeframe (1m, 1h, 1D, ...), fast with NumPy.
+        """
+
+        if start_date is None:
+            start_date = self._parse_date_for_klines(BINANCE_EARLIEST_DATE)
+
+        if end_date is None:
+            end_date = self._parse_date_for_klines(BINANCE_LATEST_DATE)
+
+        if timeframe not in TIMEFRAME_MAP:
+            raise ValueError(f"Unsupported timeframe: {timeframe}")
+
+        step_ms = int(TIMEFRAME_MAP[timeframe].total_seconds() * 1000)
+
+        return np.arange(start_date, end_date + 1, step_ms, dtype=np.int64)
--- engine/apps/data_managers/market_data_manager.py
+++ engine/apps/data_managers/market_data_manager.py
@@ -6,20 +6,15 @@ from clickhouse_driver import Client as DBClient
 from API.data_fetcher import FetchData
 from typing import Any
 from engine.apps.data_managers.clickhouse.data_manager import ClickHouseDataManager
+from engine.apps.data_managers.managers.klines_manager import KlineManager
 from utils.global_variables.GLOBAL_VARIABLES import (
     BINANCE_TRADES_LIMIT,
     SYMBOL,
     TIMEFRAME,
-    TIMEFRAME_MAP,
-    BINANCE_EARLIEST_DATE,
-    BINANCE_LATEST_DATE,
 )
-from utils.global_variables.SCHEMAS import KLINES_SCHEMA
 from tqdm import tqdm
 from utils.logger.logger import LoggerWrapper
 from utils.logger.logger import log_execution
-from dateutil.parser import parse
-from datetime import datetime
 
 
 class MarketDataManager:
@@ -32,6 +27,12 @@ class MarketDataManager:
     ):
         self.logger = LoggerWrapper(name="Market Data Manager Module", level=log_level)
         self.symbol = symbol
+        self.kline_manager = KlineManager(
+            database_client=database_client,
+            binance_client=binance_client,
+            symbol=symbol,
+            log_level=log_level,
+        )
         self.click_house_data_manager = ClickHouseDataManager(
             client=database_client, log_level=log_level
         )
@@ -86,7 +87,6 @@ class MarketDataManager:
             self._fetch_and_write_trades(fetch_ids_dictionary)
             return self.parquet_storage.read_trades()
 
-    # TODO: refactor
     @log_execution
     def get_klines(
         self,
@@ -95,91 +95,15 @@ class MarketDataManager:
         end_date: str | None = None,
         timeframe: str = TIMEFRAME,
     ):
-        if start_date:
-            start_date = self._parse_date_for_klines(start_date)
-        if end_date:
-            end_date = self._parse_date_for_klines(end_date)
-
-        present_time_in_db = pl.DataFrame(
-            self.click_house_data_manager.get_klines(
-                symbol=self.symbol,
-                timeframe=timeframe,
-                start_date=start_date,
-                end_date=end_date,
-                columns=["open_time"],
-            ),
-            schema=["open_time"],
-            orient="row",
-        )["open_time"].to_numpy()
-
-        expected_time_in_db = self._generate_expected_timestamps(
+        return self.kline_manager.get_klines(
             start_date=start_date, end_date=end_date, timeframe=timeframe
         )
-
-        timestamps_to_fetch = np.setxor1d(present_time_in_db, expected_time_in_db)
-        timestamps_to_fetch = np.sort(timestamps_to_fetch)
-
-        if timestamps_to_fetch.size > 0:
-            self.logger.info("Missing data in the dataframe. Fetching...")
-            elements = {}
-            N = BINANCE_TRADES_LIMIT
-            for i in range(0, len(timestamps_to_fetch), N):
-                from_ts = timestamps_to_fetch[i]
-                to_ts = timestamps_to_fetch[min(i + N, len(timestamps_to_fetch)) - 1]
-                elements[from_ts] = to_ts
-
-            self._fetch_and_write_klines(fetch_dictionary=elements, timeframe=timeframe)
-
-        data = pl.DataFrame(
-            self.click_house_data_manager.get_klines(
-                symbol=self.symbol,
-                timeframe=timeframe,
-                start_date=start_date,
-                end_date=end_date,
-            ),
-            schema=KLINES_SCHEMA,
-            orient="row",
-        )
-
-        return data
 
     @log_execution
     def get_order_book(self):
         pass
 
     # ---=== HELPER METHODS ===---
-    def _parse_date_for_klines(self, date: str = "22 Oct 2024"):
-        try:
-            parsed_date = parse(date)
-            timestamp_ms = int(parsed_date.timestamp() * 1000)
-            return timestamp_ms
-        except Exception as fallback_error:
-            self.logger.error(f"Failed to parse date '{date}': {fallback_error}. ")
-            raise ValueError(f"Failed to parse date '{date}': {fallback_error}. ")
-
-    @log_execution
-    def _fetch_and_write_klines(
-        self, fetch_dictionary: dict, timeframe: str = TIMEFRAME
-    ):
-        interval_ms = TIMEFRAME_MAP[timeframe]  # перетворення 1h → 3600000 ms
-        for from_ts, to_ts in tqdm(fetch_dictionary.items(), desc="Fetching klines"):
-            start = from_ts
-            while start <= to_ts:
-                data = pl.DataFrame(
-                    self.data_fetcher.fetch_historical_klines(
-                        timeframe=timeframe, start_str=start, end_str=to_ts
-                    ),
-                    orient="row",
-                    schema=KLINES_SCHEMA,
-                )
-                if len(data) == 0:
-                    break
-
-                self.click_house_data_manager.insert_klines(df=data, symbol=self.symbol)
-
-                # Переходимо до наступного batch
-                start = int(data["open_time"].max()) + interval_ms
-
     def _fetch_and_write_trades(self, fetch_ids_dictionary: dict):
         """Fetch trades from API and append them to parquet storage with retry logic."""
         for from_id, amount in fetch_ids_dictionary.items():
@@ -192,30 +116,6 @@ class MarketDataManager:
                 self.data_fetcher.fetch_historical_trades(range_id, limit)
 
     # ---=== STATIC METHODS ===---
-    def _generate_expected_timestamps(
-        self,
-        *,
-        start_date: str | None = None,
-        end_date: str | None = None,
-        timeframe: str = TIMEFRAME,
-    ) -> np.ndarray:
-        """
-        Generate expected timestamps (in ms) between start_date and end_date
-        based on timeframe (1m, 1h, 1D, ...), fast with NumPy.
-        """
-
-        if start_date is None:
-            start_date = self._parse_date_for_klines(BINANCE_EARLIEST_DATE)
-
-        if end_date is None:
-            end_date = self._parse_date_for_klines(BINANCE_LATEST_DATE)
-
-        if timeframe not in TIMEFRAME_MAP:
-            raise ValueError(f"Unsupported timeframe: {timeframe}")
-
-        step_ms = int(TIMEFRAME_MAP[timeframe].total_seconds() * 1000)
-
-        return np.arange(start_date, end_date + 1, step_ms, dtype=np.int64)
 
     @staticmethod
     def _calculate_fetch_points(amount: int, from_id: int):
