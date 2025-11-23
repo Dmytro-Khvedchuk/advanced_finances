d15bb2478be888304e653ad49d4aefe999af1898
Dmytro-Khvedchuk <dmytro.khvedchuk.dev@gmail.com>
2025-11-23 17:23:04 +0100

    feat(features): added the month/year cycle for the historical data

--- engine/apps/data_managers/scrapers/forex_factory_scraper.py
+++ engine/apps/data_managers/scrapers/forex_factory_scraper.py
@@ -1,6 +1,6 @@
 from utils.logger.logger import LoggerWrapper
 from bs4 import BeautifulSoup
-
+from utils.global_variables.GLOBAL_VARIABLES import MONTHS
 import cloudscraper
 
 
@@ -10,7 +10,7 @@ class ForexFactoryScraper:
             name="Forex Factory Scraper Module", level=log_level
         )
 
-        self.url = "https://www.forexfactory.com/"
+        self.base_url = "https://www.forexfactory.com/"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -20,6 +20,24 @@ class ForexFactoryScraper:
 
         self.scraper = cloudscraper.create_scraper()
 
-    def get_data(self):
-        response = self.scraper.get(self.url)
-        soup = BeautifulSoup(response.text, "lxml")
+    def get_data(self, from_month: int, from_year: int, duration_months: int):
+        self.run_through_months(
+            from_month=from_month,
+            from_year=from_year,
+            duration_months=duration_months
+        )
+        # response = self.scraper.get(self.url)
+        # soup = BeautifulSoup(response.text, "lxml")
+
+    def run_through_months(self, from_month: int, duration_months: int, from_year: int):
+        month = from_month
+        year = from_year
+
+        for i in range(duration_months):
+            print(f"month {i}")
+            print(f"date: {MONTHS[month]}, {year}")
+
+            month += 1
+            if month > 12:
+                month = 1
+                year += 1
